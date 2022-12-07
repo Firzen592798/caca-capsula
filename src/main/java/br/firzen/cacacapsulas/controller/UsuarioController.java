7af5c23b110b10e0d90addd353733a98f8f101ad
Augusto <augusto@imd.ufrn.br>
2022-12-06 21:30:45 -0300

    corrige controller usuario

--- src/main/java/br/firzen/cacacapsulas/controller/UsuarioController.java
+++ src/main/java/br/firzen/cacacapsulas/controller/UsuarioController.java
@@ -16,24 +16,10 @@ import br.firzen.cacacapsulas.service.UsuarioService;
 
 @Controller
 @RequestMapping("/usuario")
-public class UsuarioController {// extends AbstractController<Usuario>{
+public class UsuarioController extends AbstractController<Usuario>{
 	
 	private final String URL_PAGE= "usuario";
-
-	//protected final static String URL_PAGE = "";
 	
-	protected final static String URL_LIST = "/list";
-	
-	protected final static String URL_VIEW = "/view";
-	
-	protected final static String URL_FORM = "/form";
-	
-	protected final static String URL_EDIT = "/edit";
-	
-	protected final static String URL_DELETE = "/delete";
-	@Autowired
-	private UsuarioService service;
-
 	@GetMapping
 	public String list(Model model) {
 		model.addAttribute("usuarioLista", service.findAll());
